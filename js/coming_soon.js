a8db447e39fe5535e4d3978e13599a5b3c3ca0b9
SETeam2OARS <emsheezy.o@gmail.com>
2016-04-08 00:21:39 -0500

    Update coming_soon.js

--- js/coming_soon.js
+++ js/coming_soon.js
@@ -1,12 +1,213 @@
-$(document).ready(function(){
-   $('#filterControl .badge').click(function(){
-        oldColor = $('.cover').attr('data-color');
-        newColor = $(this).attr('data-color');
-        $('.cover').removeClass(oldColor).addClass(newColor).attr('data-color',newColor);
-        $('#filterControl .badge').each(function(){
-            $(this).removeClass('active');
-        });
-        $(this).addClass('active');
-   });
-    
-});
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="utf-8" />
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+
+    <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport' />
+    <meta name="viewport" content="width=device-width" />
+
+	<link rel="apple-touch-icon" sizes="76x76" href="/coming-sssoon-pages/images/apple-icon.png">
+	<link rel="icon" type="image/png" sizes="96x96" href="/coming-sssoon-pages/images/favicon.png">
+	
+    <title>Coming Sssoon Page by Creative Tim </title>
+
+    <meta name="keywords" content="coming soon page, under construction page, bootstrap page, free bootstrap page, creative tim, not ready page, demo page">
+    <meta name="description" content="Free one page item based on Bootstrap 3. Use Coming Sssoon Page to create a following group users before actually starting your project! ">
+
+    <!-- Schema.org markup for Google+ -->
+    <meta itemprop="name" content="Coming Sssoon Page by Creative Tim">
+    <meta itemprop="description" content="Free one page item based on Bootstrap 3. Use Coming Sssoon Page to create a following group users before actually starting your project! ">
+    <meta itemprop="image" content="http://s3.amazonaws.com/creativetim_bucket/products/20/coming_sssoon_thumbnail.png">
+
+    <!-- Twitter Card data -->
+    <meta name="twitter:card" content="summary">
+    <meta name="twitter:site" content="@creativetim">
+    <meta name="twitter:title" content="Coming Sssoon Page by Creative Tim">
+    <meta name="twitter:description" content="Free one page item based on Bootstrap 3. Use Coming Sssoon Page to create a following group users before actually starting your project! ">
+    <meta name="twitter:creator" content="@creativetim">
+    <meta name="twitter:image" content="http://s3.amazonaws.com/creativetim_bucket/products/20/coming_sssoon_thumbnail.png">
+
+    <!-- Open Graph data -->
+    <meta property="og:title" content="Coming Sssoon Page by Creative Tim" />
+    <meta property="og:type" content="article" />
+    <meta property="og:url" content="http://demos.creative-tim.com/coming-sssoon-demo-image-background" />
+    <meta property="og:image" content="http://s3.amazonaws.com/creativetim_bucket/products/20/coming_sssoon_thumbnail.png" />
+    <meta property="og:description" content="Free one page item based on Bootstrap 3. Use Coming Sssoon Page to create a following group users before actually starting your project! " />
+    <meta property="og:site_name" content="Creative Tim" />
+
+    <link href="/coming-sssoon-pages/css/bootstrap.css" rel="stylesheet" />
+	<link href="/coming-sssoon-pages/css/coming-sssoon.css" rel="stylesheet" />
+
+    <!--     Fonts     -->
+    <link href="http://netdna.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.css" rel="stylesheet">
+    <link href='http://fonts.googleapis.com/css?family=Grand+Hotel' rel='stylesheet' type='text/css'>
+
+</head>
+
+<body>
+<nav class="navbar navbar-transparent navbar-fixed-top" role="navigation">
+  <div class="container">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+    </div>
+
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul class="nav navbar-nav">
+         <li class="dropdown">
+              <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+                <img src="/coming-sssoon-pages/images/flags/US.png"/>
+                English(US)
+                <b class="caret"></b>
+              </a>
+              <ul class="dropdown-menu">
+                <li><a href="#"><img src="/coming-sssoon-pages/images/flags/DE.png"/> Deutsch</a></li>
+                <li><a href="#"><img src="/coming-sssoon-pages/images/flags/GB.png"/> English(UK)</a></li>
+                <li><a href="#"><img src="/coming-sssoon-pages/images/flags/FR.png"/> Français</a></li>
+                <li><a href="#"><img src="/coming-sssoon-pages/images/flags/RO.png"/> Română</a></li>
+                <li><a href="#"><img src="/coming-sssoon-pages/images/flags/IT.png"/> Italiano</a></li>
+
+                <li class="divider"></li>
+                <li><a href="#"><img src="/coming-sssoon-pages/images/flags/ES.png"/> Español <span class="label label-default">soon</span></a></li>
+                <li><a href="#"><img src="/coming-sssoon-pages/images/flags/BR.png"/> Português <span class="label label-default">soon</span></a></li>
+                <li><a href="#"><img src="/coming-sssoon-pages/images/flags/JP.png"/> 日本語 <span class="label label-default">soon</span></a></li>
+                <li><a href="#"><img src="/coming-sssoon-pages/images/flags/TR.png"/> Türkçe <span class="label label-default">soon</span></a></li>
+
+              </ul>
+        </li>
+
+      </ul>
+      <ul class="nav navbar-nav navbar-right">
+            <li>
+                <a href="#">
+                    <i class="fa fa-facebook-square"></i>
+                    Like
+                </a>
+            </li>
+             <li>
+                <a href="#">
+                    <i class="fa fa-twitter"></i>
+                    Tweet
+                </a>
+            </li>
+             <li>
+                <a href="#">
+                    <i class="fa fa-gittip"></i>
+                    Gittip
+                </a>
+            </li>
+       </ul>
+
+    </div><!-- /.navbar-collapse -->
+  </div><!-- /.container -->
+</nav>
+<div class="main" style="background-image: url('/coming-sssoon-pages/images/restaurant.jpg')">
+
+<!--    Change the image source '/images/restaurant.jpg')" with your favourite image.     -->
+
+    <div class="cover black" data-color="black"></div>
+
+<!--   You can change the black color for the filter with those colors: blue, green, red, orange       -->
+
+    <div class="container">
+        <h1 class="logo cursive">
+            Restaurant
+        </h1>
+<!--  H1 can have 2 designs: "logo" and "logo cursive"           -->
+
+        <div class="content">
+            <h4 class="motto">The smallest little eatery in town with the biggest taste.</h4>
+            <div class="subscribe">
+                <h5 class="info-text">
+                    Join the waiting list for the beta. We keep you posted.
+                </h5>
+                <div class="row">
+                    <div class="col-md-4 col-md-offset-4 col-sm6-6 col-sm-offset-3 ">
+                        <form class="form-inline" role="form">
+                          <div class="form-group">
+                            <label class="sr-only" for="exampleInputEmail2">Email address</label>
+                            <input type="email" class="form-control transparent" placeholder="Your email here...">
+                          </div>
+                          <button type="submit" class="btn btn-warning btn-fill">Notify Me</button>
+                        </form>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="footer">
+      <div class="container">
+             Made with <i class="fa fa-heart heart"></i> by <a href="http://www.creative-tim.com">Creative Tim</a>. Free download <a href="http://www.creative-tim.com/product/coming-sssoon-page">here.</a>
+      </div>
+    </div>
+ </div>
+
+ <div class="fixed-plugin">
+    <div class="dropdown open">
+      <a href="#" data-toggle="dropdown">
+        <i class="fa fa-cog fa-2x"> </i>
+      </a>
+       <ul class="dropdown-menu">
+          <li>
+            <a id="filterControl">
+                <span class="badge badge-black active" data-color="black"></span>
+                <span class="badge badge-blue" data-color="blue"></span>
+                <span class="badge badge-green" data-color="green"></span>
+                <span class="badge badge-orange" data-color="orange"></span>
+                <span class="badge badge-red" data-color="red"></span>
+                Filter Color
+            </a>
+        </li>
+        <li>
+            <a href="/coming-sssoon-demo-image-background">
+                <img src="/coming-sssoon-pages/images/thumb_image.png">
+                 Image Background
+            </a>
+        </li>
+        <li>
+            <a href="/coming-sssoon-demo-video-background">
+                <img src="/coming-sssoon-pages/images/thumb_video.png">
+                 Video Background
+            </a>
+        </li>
+        <li >
+            <a href="/coming-sssoon-demo-cream-app">
+               <img src="/coming-sssoon-pages/images/thumb_application.png">
+               Application Page
+            </a>
+        </li>
+         <li class="active">
+            <a href="/coming-sssoon-demo-restaurant">
+               <img src="/coming-sssoon-pages/images/thumb_restaurant.png">
+              Restaurant Page
+            </a>
+        </li>
+		<li class="header text-center">
+ 		   <h5>Free Download</h5>
+ 	   </li>
+ 	   <li>
+ 		   <a href="http://www.creative-tim.com/product/coming-sssoon-page" class="btn btn-warning btn-fill"><i class="fa fa-html5"></i> HTML Template</a>
+ 	   </li>
+ 	   <li>
+ 		   <a href="http://www.creative-tim.com/product/coming-sssoon-page-wordpress-plugin" class="btn btn-info btn-fill"><i class="fa fa-wordpress"></i> WordPress Plugin</a>
+ 	   </li>
+        <li><a></a></li>
+      </ul>
+
+    </div>
+ </div>
+</body>
+    <script src="/coming-sssoon-pages/js/jquery-1.10.2.js" type="text/javascript"></script>
+	<script src="/coming-sssoon-pages/js/bootstrap.min.js" type="text/javascript"></script>
+	<script src="/coming-sssoon-pages/js/coming-sssoon.js" type="text/javascript"></script>
+
+
+</html>
